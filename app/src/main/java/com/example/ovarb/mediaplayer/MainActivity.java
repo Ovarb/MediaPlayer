715a41c3cb0af28305b3928bdfcfd3bd6dd96aa4
vsevolod.alexeev <vsevolod.alexeev@osinit.com>
2018-08-01 16:40:53 +0300

    onClick Listeners are assigned to the buttons

--- app/src/main/java/com/example/ovarb/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/ovarb/mediaplayer/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.ovarb.mediaplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find playbutton
+        Button buttonPlay = (Button) findViewById(R.id.button_play);
+
+        //set playbutton onclick listener
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 displayMessage("Play is pressed");
+             }
+        });
+
+
+
+        //find pausebutton
+        Button buttonPause = (Button) findViewById(R.id.button_pause);
+
+        //set pausebutton onclick listener
+        buttonPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayMessage("Pause is pressed");
+            }
+        });
+
+
+
+
+
+
+
+
+    }
+
+    private void displayMessage(String message) {
+        //find display_action
+        TextView displayAction = (TextView) findViewById(R.id.display_action);
+        displayAction.setText(message);
     }
 }

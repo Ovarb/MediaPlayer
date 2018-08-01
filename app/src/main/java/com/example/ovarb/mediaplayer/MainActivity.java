806e65cb77c1ac3ad22c3fd85c06f3a77ea23c20
Vsevolod Alexeev <ovarb6@gmail.com>
2018-08-02 02:45:51 +0300

    song duration variable is added

--- app/src/main/java/com/example/ovarb/mediaplayer/MainActivity.java
+++ app/src/main/java/com/example/ovarb/mediaplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ovarb.mediaplayer;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -31,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
              @Override
              public void onClick(View v) {
                  displayMessage("Play is pressed");
+
+                 int songDuration = player.getDuration();
+                 displayMessage("" + songDuration);
                  playSong();
+
              }
         });
 
